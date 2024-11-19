f71ae69916b3cd7366787f2cb9d0bf4c26149c04
Farhan Nawaz <130452191+farhannawazofficial@users.noreply.github.com>
2024-11-19 16:48:12 +0500

    Initial commit

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "b4bb8c5f8c690a197d64e9dcbaec84c3",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
